2cbf78619058518116dcd77bf66974d5baf6fdc3
ezoehunt <ezoehunt@gmail.com>
2017-03-13 21:32:12 -0700

    tab flaps

--- e/wp-content/themes/ehtheme/content-page.php
+++ e/wp-content/themes/ehtheme/content-page.php
@@ -61,61 +61,37 @@
 </div>
 
 
-<!-- controls -->
-<div class="testrow" style="">
 
-  <div id="" class="testdiv" style="width:20%;border:2px solid blue;">
-  </div>
 
-  <div id="" class="testdiv" style="width:30%;border:2px solid blue;background-color:fuchsia;color:black;text-align:center;padding:.25rem;">
-    Viewing Design
-  </div>
 
-  <div id="" class="testdiv" style="width:30%;border:2px solid blue;background-color:white;color:fuchsia;text-align:center;padding:.25rem;">
-    Switch to Process
-  </div>
 
-  <div id="" class="testdiv" style="width:20%;border:2px solid blue;">
+<!-- PAGE CONTENT -->
+<!--div class="" style="width: 100%;"-->
+
+<div id="" class="testdiv" style="width:20%;border:2px solid blue;">
+</div>
+
+<div class="testdiv" style="width:60%;background-color:white;padding:2rem; border:2px solid blue;">
+
+  <div id="post-content" style="width:">
+    <p><?php the_content();?></p>
   </div>
 
 </div>
 
+<div class="testdiv" style="width:10%;border:2px solid blue;">
 
-<!-- title content -->
-<div class="testrow post-content-row" style="">
-
-  <div id="" class="testdiv" style="width:20%;">
+  <div style="background-color:yellow;color:black;padding:.5rem;">
+    View Design
   </div>
 
-  <div id="" class="testdiv" style="width:70%;background-color:yellow;padding:.5rem;"><!--call this content-block-->
-
-    <div id="post-content" style="width:">
-      <p>Club Wedd Home Page - Logged Out</p>
-    </div>
-
-  </div>
-
-  <div id="" class="testdiv" style="width:10%;">
+  <div style="background-color:fuchsia;color:black;padding:.5rem;">
+    View Process
   </div>
 
 </div>
 
-
-<!-- content -->
-<div class="testrow post-content-row" style="">
-
-  <div id="" class="testdiv" style="width:20%;">
-  </div>
-
-  <div id="" class="testdiv" style="width:60%;background-color:#FFFFFF;padding: 1rem 2rem 1rem 2rem;"><!--call this content-block-->
-
-    <div id="post-content" style="width:">
-      <p><?php the_content();?></p>
-    </div>
-
-  </div>
-
-  <div id="" class="testdiv" style="width:20%;">
-  </div>
-
+<div class="testdiv" style="width:10%;border:2px solid blue;">
 </div>
+
+<!--/div-->
